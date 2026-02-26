8def10d0a4cd44fa50254e133ed49897ff4f3875
aacadavid <aacadavid@unicesar.edu.co>
2026-02-25 21:12:52 -0500

    commit: version 0 del programa

---
+++ src/biblioteca/Libro.java
@@ -0,0 +1,122 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package biblioteca;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alvar
+ */
+public class Libro {
+    
+    private String titulo;
+    private String autor;;
+    private String codigo;
+    private boolean disponible;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, String codigo, boolean disponible) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.codigo = codigo;
+        this.disponible = disponible;
+    }
+    
+    public boolean prestarLibro(boolean disponible){
+        
+        if (disponible==true) {
+            
+            disponible=false;
+            
+            JOptionPane.showMessageDialog(null, "El libro se ha prestado");
+            
+                    
+            return disponible;
+            
+        } else {
+            JOptionPane.showMessageDialog(null, "Este libro está ocupado");
+        }
+        return false;
+        
+    }
+    
+    public boolean devolverLibro(boolean disponible){
+        
+        if (disponible==false) {
+            
+            disponible=true;
+            
+            JOptionPane.showMessageDialog(null, "El libro se ha devuelto");
+            
+                    
+            return disponible;
+            
+        } else {
+            JOptionPane.showMessageDialog(null, "Este libro no se ha prestado");
+        }
+        return false;
+        
+    }
+    
+    public String mostrarInfo(){
+        
+        String x;
+        
+        if (disponible=true) {
+            
+            x ="Disponible";
+            
+        } else {
+            
+            x="No disponible";
+            
+        }
+        
+           return "Titulo del libro: " + titulo + "\n" +
+                  "Autor del libro: " + autor + "\n" +
+                  "Codigo del libro: " + codigo + "\n" + 
+                  "Estado: " + x;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    public void setDisponible(boolean disponible) {
+        this.disponible = disponible;
+    }
+    
+    
+    
+    
+    
+}
---
+++ src/biblioteca/Menu.java
@@ -0,0 +1,155 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package biblioteca;
+
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alvar
+ */
+public class Menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int op = 0;
+        
+        String a;
+        
+        boolean encontrado = false;
+        
+        Libro unidad;
+        
+        ArrayList<Libro> libros=new ArrayList<>();
+        
+        String autor,titulo,codigo;
+        boolean estado;
+        
+        while (op!=6) {            
+            
+            op=Integer.parseInt(JOptionPane.showInputDialog("Ingrese una opcion disponible en el menú" + "\n" +
+                                                        "1 Registrar libro " + "\n" + 
+                                                        "2 Prestar libro " + "\n" +
+                                                        "3 Devolver libro " + "\n" + 
+                                                        "4 Mostrar libros disponible" + "\n" +
+                                                        "5 Buscar libro por codigo " + "\n" + 
+                                                        "6 Salir"));
+            
+            switch (op) {
+            case 1:
+                
+                codigo=JOptionPane.showInputDialog("Ingrese el codigo del libro");
+                titulo=JOptionPane.showInputDialog("Ingrese el titulo del libro");
+                autor=JOptionPane.showInputDialog("Ingrese el nombre del autor del libro");
+                estado=true;
+                
+                unidad=new Libro(titulo, autor, codigo, estado);
+                
+                libros.add(unidad);
+                
+                JOptionPane.showMessageDialog(null, "Libro resgistrado exitosamente");
+                
+                break;
+                
+            case 2:
+                
+                a=JOptionPane.showInputDialog("Ingrese el codigo del libro a prestar");
+                
+                for (Libro l : libros) {
+                    
+                    if (a.equals(l.getCodigo())) {
+                        
+                        encontrado=true;
+                        
+                        l.setDisponible(false);
+                        
+                        JOptionPane.showMessageDialog(null, "El libro " + l.getTitulo() + "ha sido prestado");
+                        
+                    } 
+                    
+                }
+                
+                if (encontrado==false) {
+                    JOptionPane.showMessageDialog(null, "No se encontró el libro");
+                }
+                
+                break;
+                
+            case 3:
+                
+                a=JOptionPane.showInputDialog("Ingrese el codigo del libro a devolver");
+                
+                for (Libro l : libros) {
+                    
+                    if (a.equals(l.getCodigo())) {
+                        
+                        encontrado=true;
+                        
+                        l.setDisponible(true);
+                        
+                        JOptionPane.showMessageDialog(null, "El libro " + l.getTitulo() + "ha sido devuelto");
+                        
+                    } 
+                    
+                }
+                
+                if (encontrado==false) {
+                    JOptionPane.showMessageDialog(null, "No se encontró el libro");
+                }
+                
+                break;
+                
+            case 4:
+                
+                for (Libro l2 : libros) {
+                    
+                    JOptionPane.showMessageDialog(null, "=============================================");    
+                    JOptionPane.showMessageDialog(null, l2.mostrarInfo());
+                    JOptionPane.showMessageDialog(null, "=============================================");
+                    
+                }
+                
+                break;
+                
+            case 5:
+                
+                a=JOptionPane.showInputDialog("Ingrese el codigo del libro a buscar");
+                
+                for (Libro l : libros) {
+                    
+                    if (a.equals(l.getCodigo())) {
+                        
+                        encontrado=true;
+                        
+                        JOptionPane.showMessageDialog(null, l.mostrarInfo() );
+                        
+                    } 
+                    
+                }
+                
+                if (encontrado==false) {
+                    JOptionPane.showMessageDialog(null, "No se encontró el libro");
+                }
+                
+                break;
+                
+            case 6:
+                
+                JOptionPane.showMessageDialog(null, "PROGRAMA FINALIZADO");
+                
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Ingrese una opcion disponible");
+        }
+            
+       }
+        
+    }
+    
+}
